08a8e45dd0e6bd2659a02cd853f0d00577af5a2f
s1310022 <s1310022@u-aizu.ac.jp>
2024-06-28 12:09:59 +0900

    HoT: initial revision

---
+++ coin.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+  int a=0,b=0;
+  char name[100];
+  printf("What are you?\n");
+  scanf("%s",name);
+  printf("Hello, %s!\n",name);
+  printf("Tossing a coin...\n");
+  srand((int)time(NULL));
+  fot(int i=0;i<3;i++){
+    printf("Round %d : ",i);
+    if(rand()%2==0){
+      printf("Heads\n");
+      a++;
+    }
+    else{
+      printf("Tails\n");
+      b++;
+    }
+  }
+    printf("Heads : %d, Tails : %d\n",a,b);
+    if(a>b){
+      printf("%s won\n",name);
+    }
+    else{
+      printf("%s lost\n",name);
+    }
+    return 0;
+}
+  
